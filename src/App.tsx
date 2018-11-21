666f3c0192f556bc87023ecb6887fb4e9f6b247b
wslyvh <wslyvh@users.noreply.github.com>
2018-11-21 16:46:57 +0100

    add tslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  render() {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
