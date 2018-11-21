99d20fb9168896bee461e2d92f2ed76b9b4c0f8f
wslyvh <wslyvh@users.noreply.github.com>
2018-11-21 16:31:54 +0100

    add express server

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// API calls
+app.get('/api/repos', (req, res) => {
+  var repos = [ "Repo #1", "Repo #2" ];
+  res.json(repos);
+});
+
+app.get('/api/issues', (req, res) => {
+    var issues = [ "Issue #1", "Issue #2" ];
+    res.json(issues);
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/build', 'index.html'));
+  });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
