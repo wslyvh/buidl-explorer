efd014c0a6b40fe166af8fec3159354221b72202
wslyvh <wslyvh@users.noreply.github.com>
2018-11-22 10:50:57 +0100

    restructure

--- index.js
+++ index.js
@@ -21,11 +21,11 @@ app.get('/api/issues', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, '/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
   });
 }
 
