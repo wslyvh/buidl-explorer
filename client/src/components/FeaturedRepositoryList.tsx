52d4f1fba1b93bfb408f6b90a103d6ddc92b7c60
wslyvh <wslyvh@users.noreply.github.com>
2018-11-23 19:58:22 +0100

    minor styling

--- client/src/components/FeaturedRepositoryList.tsx
+++ client/src/components/FeaturedRepositoryList.tsx
@@ -30,7 +30,7 @@ class FeaturedRepositoryList extends Component {
             <Divider><h2>Featured Repositories</h2></Divider>
 
             <List
-                grid={{ gutter: 16, xs: 1, sm: 1, md: 2, lg: 2, xl: 3, xxl: 4}}
+                grid={{ gutter: 16, xs: 1, sm: 1, md: 2, lg: 2, xl: 4, xxl: 4}}
                 dataSource={this.state.repos} renderItem={(repo: any) => (
                     <List.Item>
                         <RepositoryCard repository={repo} />
