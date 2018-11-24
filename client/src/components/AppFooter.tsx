c5f939b0ec7f9e5a20b7516c130e0146f364f27f
wslyvh <wslyvh@users.noreply.github.com>
2018-11-24 19:55:51 +0100

    analytics

--- client/src/components/AppFooter.tsx
+++ client/src/components/AppFooter.tsx
@@ -4,10 +4,14 @@ import React, { Component } from "react";
 class AppFooter extends Component {
   public render() {
     return (
-        <div className="footer-icons">
-          <a href="https://twitter.com/wslyvh"><Icon type="twitter" /></a>
-          <a href="https://github.com/wslyvh/buidl-explorer"><Icon type="github" /></a>
-        </div>
+      <div className="footer-icons">
+        <a href="https://twitter.com/wslyvh">
+          <Icon type="twitter" /> <small>follow</small>
+        </a>
+        <a href="https://github.com/wslyvh/buidl-explorer/issues/new">
+          <Icon type="github" /> <small>feedback</small>
+        </a>
+      </div>
     );
   }
 }
