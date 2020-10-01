1296eb42a94806598ebbcde05b4208a6fda39c38
wslyvh <wslyvh@users.noreply.github.com>
2020-10-01 17:18:17 +0200

    typo

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -8,7 +8,7 @@ class AppHeader extends Component {
           <a href="/">BUIDL Explorer</a>
         </h1>
         <p>
-          Starting with open-source projects can be freighting, especially in a fast moving space as blockchain. <br />
+          Starting with open-source projects can be frightening, especially in a fast moving space as blockchain. <br />
           We'll help you get started and find the right open-source Ethereum projects to contribute to...
           </p>
         <p>
